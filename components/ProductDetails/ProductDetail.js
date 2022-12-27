7471b8f4fd17ee93d98b19fb9c8119ad84f24a5c
doganeraylin <doganeraylinn@gmail.com>
2022-12-27 10:24:34 +0200

    added class name to img

--- components/ProductDetails/ProductDetail.js
+++ components/ProductDetails/ProductDetail.js
@@ -10,16 +10,16 @@ const ProductDetails = () => {
     return (
         <>  
             <Header />
-            <div className={styles.productDetailsContainer}>
-            <img src="/book-cover.jpg"></img>
-            <div className={styles.productTextContainer}>
-                 <p className={styles.heartIconSolid}>5 likes<span><FontAwesomeIcon icon={faFileSolid} className={styles.heartIconSolid}/></span></p>
-                 <p className={styles.productName}>The Art of Computer Programming</p>
-                 <p className={styles.productDescription}>he Art of Computer Programming (TAOCP) is a comprehensive monograph written by the computer scientist Donald Knuth presenting programming algorithms and their analysis. Volumes 1–5 are intended to represent the central core of computer programming for sequential machines.
-                 When Knuth began the project in 1962, he originally conceived of it as a single book with twelve chapters. The first three volumes of what was then expected to be a seven-volume set were published in 1968, 1969, and 1973. Work began in earnest on Volume 4 in 1973, but was suspended in 1977 for work on typesetting prompted by the second edition of Volume 2. Writing of the final copy of Volume 4A began in longhand in 2001, and the first online pre-fascicle, 2A, appeared later in 2001</p>
-                 <p className={styles.productPrice}>35.00 ₺</p>
-            </div>
-        </div>
+                <div className={styles.productDetailsContainer}>
+                    <img src="/book-cover.jpg" className={styles.productImg}></img>
+                    <div className={styles.productTextContainer}>
+                        <p className={styles.heartIconSolid}>5 likes<span><FontAwesomeIcon icon={faFileSolid} className={styles.heartIconSolid}/></span></p>
+                        <p className={styles.productName}>The Art of Computer Programming</p>
+                        <p className={styles.productDescription}>he Art of Computer Programming (TAOCP) is a comprehensive monograph written by the computer scientist Donald Knuth presenting programming algorithms and their analysis. Volumes 1–5 are intended to represent the central core of computer programming for sequential machines.
+                        When Knuth began the project in 1962, he originally conceived of it as a single book with twelve chapters. The first three volumes of what was then expected to be a seven-volume set were published in 1968, 1969, and 1973. Work began in earnest on Volume 4 in 1973, but was suspended in 1977 for work on typesetting prompted by the second edition of Volume 2. Writing of the final copy of Volume 4A began in longhand in 2001, and the first online pre-fascicle, 2A, appeared later in 2001</p>
+                        <p className={styles.productPrice}>35.00 ₺</p>
+                    </div>
+                </div>
         </> 
     )
 }
