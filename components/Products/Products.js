ccc1c37cea87d866edab3b01c8a382e1292e1517
doganeraylin <doganeraylinn@gmail.com>
2022-12-27 08:21:29 +0200

    added next link

--- components/Products/Products.js
+++ components/Products/Products.js
@@ -1,4 +1,5 @@
 import axios from "axios"
+import  Link  from "next/link"
 import styles from "./Products.module.scss"
 import Header from "../Header/Header"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -7,21 +8,17 @@ import { faHeart as faFileSolid } from "@fortawesome/free-solid-svg-icons"
 import { faHeart as faFileRegular } from "@fortawesome/free-regular-svg-icons"
 import { useEffect, useState } from "react";
 
-
-
-
 const Products = () => {
 
     const accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InN0cmluZyIsImlhdCI6MTY0OTg2MDExMiwiZXhwIjoxNjc1NzgwMTEyfQ.z8XYELsP1GBKkGpyvI14WzJKQAAbtQUwCl3hlLs_U4M"
     const baseURL = 'https://assignment-api.piton.com.tr/api/v1/product/all';
-    const [products, setProducts] = useState(null);
+    const [products, setProducts] = useState([]);
 
     const fetchData = async () => {
         try {
         const config = {
             headers: {
                 "access-token" : accessToken
-                // Authorization: `Bearer ${accessToken}`
             }
         };
         const response = await axios.get(baseURL, config);
@@ -32,7 +29,6 @@ const Products = () => {
         }
     };
   
-
 useEffect(() => {
     fetchData()
 }, [])
@@ -40,41 +36,29 @@ useEffect(() => {
     return (
         <>
         <Header />
+
         <div className={styles.productList}>
             {products.map((product) => {
                 return (
-                    <div className={styles.productContainer}>
-                        <FontAwesomeIcon icon={faFileRegular} className={styles.heartIconRegular}/>
-                        <img src={product.image}className={styles.productImg}></img>
-                        <p className={styles.productName}>{product.name}</p>
-                        <hr></hr>
-                        <p className={styles.productPrice}>{`${product.price} ₺`}</p>
-                    </div>
+                    <Link href={"/products/" + product.id} key={product.id}>
+                        <div className={styles.productContainer}>
+                            <FontAwesomeIcon icon={faFileRegular} className={styles.heartIconRegular}/>
+                            <a>
+                                <img src={product.image}className={styles.productImg}></img>
+                            </a>
+                         
+                            <p className={styles.productName}>{product.name}</p>
+                            <hr></hr>
+                            <p className={styles.productPrice}>{`${product.price} ₺`}</p>
+                        </div>
+                    </Link>
                 )
             })}
-        </div>
+        </div> 
         </>
     )
 }
 
 export default Products
-
-
-// useEffect(() => {
-
-//     const config = {
-//       headers: {
-//         Authorization: `Bearer ${accessToken}`
-//       }
-//     };
-
-//     axios.get(baseURL, config)
-//         .then((response) => {
-//         setPost(response.data);
-//     });
-//   }, []);
-
-//      if (!post) return null;
- 
 
 
