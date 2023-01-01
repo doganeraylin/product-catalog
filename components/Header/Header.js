667caf4f4b652b3881782406add0b4b6363ec790
doganeraylin <doganeraylinn@gmail.com>
2023-01-01 15:12:58 +0200

    logout function added

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,10 +1,18 @@
 import styles from "./Header.module.scss"
+import { useRouter } from "next/router"
 
 const Header = () => {
+
+    const router = useRouter()
+
+    const logOut = () => {
+        router.push('/login')
+    }
+
     return (
         <div className={styles.headerContainer}>
             <h1 className={styles.logo}>Piton<span className={styles.logoSpan}>Shop</span></h1>
-            <button className={styles.logOut}>Logout</button>
+            <button className={styles.logOut} onClick={logOut}>Logout</button>
         </div>
     )
 }
