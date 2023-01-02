c5c83aaf5e0ec4f40a45f0fb6140b8847d47bc69
doganeraylin <doganeraylinn@gmail.com>
2023-01-02 14:15:05 +0200

    rendered product details

--- components/ProductDetails/ProductDetail.js
+++ components/ProductDetails/ProductDetail.js
@@ -1,25 +1,50 @@
-import styles from "./ProductDetails.module.scss"
 import Header  from "../Header/Header"
+import styles from "./ProductDetails.module.scss"
+import { useState, useEffect } from "react";
+import axios from "axios"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
 import {faHeart as faFileSolid} from "@fortawesome/free-solid-svg-icons"
-
 import {faHeart as faFileRegular} from "@fortawesome/free-regular-svg-icons"
 
-const ProductDetails = () => {
+const ProductDetails = (props) => {
+
+    const accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InN0cmluZyIsImlhdCI6MTY0OTg2MDExMiwiZXhwIjoxNjc1NzgwMTEyfQ.z8XYELsP1GBKkGpyvI14WzJKQAAbtQUwCl3hlLs_U4M"
+    const baseURL = 'https://assignment-api.piton.com.tr/api/v1/product/all';
+    const [products, setProducts] = useState([]);
+
+    const fetchData = async () => {
+        try {
+        const config = {
+            headers: {
+                "access-token" : accessToken
+            }
+        };
+        const response = await axios.get(baseURL, config);
+        setProducts(response.data.products)
+        } catch (error) {
+        console.log(error.response);
+        }
+    };
+  
+    useEffect(() => {
+        fetchData()
+    }, [])
+
+
+
     return (
         <>  
             <Header />
-                <div className={styles.productDetailsContainer}>
-                    <img src="/book-cover.jpg" className={styles.productImg}></img>
-                    <div className={styles.productTextContainer}>
-                        <p className={styles.heartIconSolid}>5 likes<span><FontAwesomeIcon icon={faFileSolid} className={styles.heartIconSolid}/></span></p>
-                        <p className={styles.productName}>The Art of Computer Programming</p>
-                        <p className={styles.productDescription}>he Art of Computer Programming (TAOCP) is a comprehensive monograph written by the computer scientist Donald Knuth presenting programming algorithms and their analysis. Volumes 1–5 are intended to represent the central core of computer programming for sequential machines.
-                        When Knuth began the project in 1962, he originally conceived of it as a single book with twelve chapters. The first three volumes of what was then expected to be a seven-volume set were published in 1968, 1969, and 1973. Work began in earnest on Volume 4 in 1973, but was suspended in 1977 for work on typesetting prompted by the second edition of Volume 2. Writing of the final copy of Volume 4A began in longhand in 2001, and the first online pre-fascicle, 2A, appeared later in 2001</p>
-                        <p className={styles.productPrice}>35.00 ₺</p>
-                    </div>
+                <div className={styles.productDetailsContainer} key={props.id}>
+                <img src={props.img} className={styles.productImg}></img>
+                <div className={styles.productTextContainer}>
+                    <p className={styles.heartIconSolid}>{props.likes} likes<span><FontAwesomeIcon icon={faFileSolid} className={styles.heartIconSolid}/></span></p>
+                    <p className={styles.productName}>{props.name}</p>
+                    <p className={styles.productDescription}>{props.description}</p>
+                    <p className={styles.productPrice}>{props.price} ₺</p>
                 </div>
+            </div>
+                
         </> 
     )
 }
