ae525fc33c90943d1fc6b6d21a829573caa9d6d2
doganeraylin <doganeraylinn@gmail.com>
2023-01-21 16:57:04 +0200

    added redirect to the main page function

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -9,9 +9,14 @@ const Header = () => {
         router.push('/login')
     }
 
+    const redirectToMainPage = () => {
+        console.log("clicked")
+        router.push('/products')
+    }
+
     return (
         <div className={styles.headerContainer}>
-            <h1 className={styles.logo}>Piton<span className={styles.logoSpan}>Shop</span></h1>
+            <h1 className={styles.logo} onClick={redirectToMainPage}>Piton<span className={styles.logoSpan}>Shop</span></h1>
             <button className={styles.logOut} onClick={logOut}>Logout</button>
         </div>
     )
