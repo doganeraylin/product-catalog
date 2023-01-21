a683b8dc8f45d68a6e66917fd597c45ff88f3bc1
doganeraylin <doganeraylinn@gmail.com>
2023-01-21 16:57:31 +0200

    removed commented out code

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,12 @@
 import Head from 'next/head'
 import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.scss'
-import Login from '../components/Login/Login'
 import Register from '../components/Register/Register'
-import Header from "../components/Header/Header"
-import Products from '../components/Products/Products'
-import ProductDetails from "../components/ProductDetails/ProductDetail"
+
 
 export default function Home() {
   return (
     <>
     <Register />  
-      {/* <Login /> */}
-       {/* <Products />  */}
-       {/* <ProductDetails />     */}
     </>
   )
 }
